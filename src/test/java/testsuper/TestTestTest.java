bc00a7ecc1bd8310588543d04d7c595550b77347
OLudman <olgaludman@gmail.com>
2022-03-21 21:12:02 +0200

    mobileApp

---
+++ src/test/java/testsuper/TestTestTest.java
@@ -0,0 +1,11 @@
+package testsuper;
+
+import manager.Configuration;
+import org.testng.annotations.Test;
+
+public class TestTestTest extends Configuration {
+    @Test
+    public void ytetstete(){
+
+    }
+}
